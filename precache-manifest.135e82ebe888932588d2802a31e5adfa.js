bbe4521f314ef315d510a97810f7f6d08b393c46
JonHualde <jonhualde94@gmail.com>
2020-07-28 21:56:08 +0200

    Updates

---
+++ precache-manifest.135e82ebe888932588d2802a31e5adfa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0222d81ce86a2d7a725052fd34a208d9",
+    "url": "/list-of-projects/index.html"
+  },
+  {
+    "revision": "02e7d61694a1a9e35340",
+    "url": "/list-of-projects/static/css/main.fd89a02f.chunk.css"
+  },
+  {
+    "revision": "bfd1b9a9437fba066168",
+    "url": "/list-of-projects/static/js/2.2207ad05.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/list-of-projects/static/js/2.2207ad05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02e7d61694a1a9e35340",
+    "url": "/list-of-projects/static/js/main.b46fa2e7.chunk.js"
+  },
+  {
+    "revision": "dc777ea86b5b42ec5732",
+    "url": "/list-of-projects/static/js/runtime-main.18d503f8.js"
+  }
+]);
